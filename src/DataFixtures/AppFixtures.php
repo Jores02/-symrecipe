0398f29f9792a294a022fb20d8f0311f345452d5
Amen AHOUANDOGBO <aa290627@iut21.u-bourgogne.fr>
2025-05-19 17:25:37 +0200

    Fixtures et FakerPHP

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Entity\Ingredient;
+use Faker\Generator; 
+use Faker\Factory; 
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    private Generator $faker; 
+    public   function __construct() {
+        $this->faker = Factory::create(locale: 'fr_FR');
+    }
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 50; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+            ->setPrice(mt_rand(1,199));
+            $manager->persist($ingredient);
+            }
+
+        $manager->flush();
+    }
+}
