dd5426c2ec37b1412bd3161bbad46087fb6c0526
AHOUANDOGBO Amen <112037382+Jores02@users.noreply.github.com>
2025-06-01 21:35:26 +0200

    Partie 3 : CRUD des recettes - Liste des recettes

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'recipe.index')]
+    public function index(RecipeRepository $recipeRepository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $recipes = $paginator->paginate(
+            $recipeRepository->findAll(),
+            $request->query->getInt('page', 1),
+            10 // nombre d’items par page
+        );
+
+        return $this->render('pages/recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
+    }
+}
